7c75a4d64a5f2bb40d75043176eb8e5821727240
Taekwon <dev.taekwonlee@gmail.com>
2022-08-20 15:37:58 +0900

    [Node] 모델링 구현

---
+++ models/companies.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const Company = sequelize.define("Company",
+    {
+      company_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+    },
+    {
+      charset: "utf8",
+      collate: "utf8_general_ci",
+      modelName: "Company",
+      tableName: "companies",
+      underscored: true,
+      freezeTableName: true,
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  Company.associate = (db) => {
+    Company.hasMany(db.JobOpening, {
+      as: "JopOpenings",
+      foreignKey: {
+        name: "fkCompanyId",
+        allowNull: false,
+      },
+      constraints: true,
+      onDelete: "cascade",
+    });
+  };
+  return Company;
+};
